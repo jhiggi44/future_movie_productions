fc0687f8a0b24556d7991688886b1ec63684c0a7
jhiggi44 <jordandhiggins@gmail.com>
2021-01-15 12:41:55 -0700

    Add trailer layout for projects

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,50 @@
 import Head from 'next/head'
+import projects from './data.js'
+
+function convertTitleToPath(title) {
+  return title.toLowerCase().split(" ").join("-");
+}
+
+const InfoField = ({header, info}) => (
+  <p><span>{ header }: </span>{ info }</p>
+)
+
+const Summary = ({children}) => (
+  <p>{ children }</p>
+)
+
+function TrailerLayout({ project, children }) {
+  return (
+    <div>
+      <a>
+        <h2>{ `${project.title} (${project.yearOfRelease})` }</h2>
+      </a>
+      <Summary>{ project.summary }</Summary>
+      <InfoField 
+        header="Director" 
+        info={ project.director } 
+      />
+      <InfoField 
+        header="Writer" 
+        info={ project.writer } 
+      />
+      <InfoField 
+        header="Stars" 
+        info={ project.cast.join(", ") }
+      />
+
+      { children }
+
+      <iframe src={ project.trailerEmbed }></iframe>
+    </div>
+  )
+}
 
 function Main() {
   return (
     <main>
-      THE MAIN SECTION
+      <TrailerLayout project={ projects["mabelAndGeorge"] } />
+      <TrailerLayout project={ projects["thistlewits"] } />
     </main>
   )
 }
