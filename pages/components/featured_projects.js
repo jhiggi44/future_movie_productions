4b60edfd908485023c3ccadc5ed908c4e121ce40
jhiggi44 <jordandhiggins@gmail.com>
2021-01-15 12:56:02 -0700

    Add FeaturedProjects section

---
+++ pages/components/featured_projects.js
@@ -0,0 +1,44 @@
+function convertTitleToPath(title) {
+    return title.toLowerCase().split(" ").join("-");
+  }
+  
+const InfoField = ({header, info}) => <p><span>{ header }: </span>{ info }</p>
+
+const Summary = ({children}) => <p>{ children }</p>
+
+function TrailerLayout({ project, children }) {
+    return (
+        <div>
+        <a>
+            <h2>{ `${project.title} (${project.yearOfRelease})` }</h2>
+        </a>
+        <Summary>{ project.summary }</Summary>
+        <InfoField 
+            header="Director" 
+            info={ project.director } 
+        />
+        <InfoField 
+            header="Writer" 
+            info={ project.writer } 
+        />
+        <InfoField 
+            header="Stars" 
+            info={ project.cast.join(", ") }
+        />
+
+        { children }
+
+        <iframe src={ project.trailerEmbed }></iframe>
+        </div>
+    )
+}
+
+function  FeaturedProjects({ projects }) {
+    return(
+        <div>
+            { projects.map((project) => <TrailerLayout project={project} />) }
+        </div>
+    )
+}
+
+export default FeaturedProjects
--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,13 @@
 import Head from 'next/head'
+import FeaturedProjects from './components/featured_projects'
 import projects from './data.js'
 
-function convertTitleToPath(title) {
-  return title.toLowerCase().split(" ").join("-");
-}
-
-const InfoField = ({header, info}) => (
-  <p><span>{ header }: </span>{ info }</p>
-)
-
-const Summary = ({children}) => (
-  <p>{ children }</p>
-)
-
-function TrailerLayout({ project, children }) {
-  return (
-    <div>
-      <a>
-        <h2>{ `${project.title} (${project.yearOfRelease})` }</h2>
-      </a>
-      <Summary>{ project.summary }</Summary>
-      <InfoField 
-        header="Director" 
-        info={ project.director } 
-      />
-      <InfoField 
-        header="Writer" 
-        info={ project.writer } 
-      />
-      <InfoField 
-        header="Stars" 
-        info={ project.cast.join(", ") }
-      />
-
-      { children }
-
-      <iframe src={ project.trailerEmbed }></iframe>
-    </div>
-  )
-}
-
+// TODO: create a route for all_projects
+// TODO: create route for featured_projects
 function Main() {
   return (
     <main>
-      <TrailerLayout project={ projects["mabelAndGeorge"] } />
-      <TrailerLayout project={ projects["thistlewits"] } />
+      <FeaturedProjects projects={ [projects["mabelAndGeorge"], projects["thistlewits"]]} />
     </main>
   )
 }
