1ef5515286cdd9cac5e40eacdda263df4e0d1908
jhiggi44 <jordandhiggins@gmail.com>
2021-01-15 14:10:05 -0700

    Add Montserrat and Open google fonts

--- pages/components/section_container.js
+++ pages/components/section_container.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 const Section = styled.section`
   max-width: 991px;
   margin: 24px auto;
+  font-family: 'Open Sans', sans-serif;
 `;
 
 const SectionContainer = ({ children }) => (
--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,8 @@ export default function Home() {
     <div>
       <Head>
         <title>Future Movie Productions</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&family=Open+Sans:ital,wght@0,300;0,400;0,700;1,400&display=swap" rel="stylesheet"/>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Main />
